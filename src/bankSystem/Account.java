47c10a455a0a539a025989a6b2d7620c68a2ed3a
Jamie95187 <jamiewong63@googlemail.com>
2020-03-29 05:05:23 +0100

    add account class and test for one deposit

--- src/bankSystem/Account.java
+++ src/bankSystem/Account.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 public class Account {
 	TransactionHistory transactionHistoryFieldVariable;
 	
-	Account(TransactionHistory transactionHistory) {
+	public Account(TransactionHistory transactionHistory) {
 		transactionHistoryFieldVariable = transactionHistory;
 	}
 	
--- src/testing/AccountTest.java
+++ src/testing/AccountTest.java
@@ -1,32 +1,52 @@
 package testing;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.mock;
 
+import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import bankSystem.Account;
 import bankSystem.TransactionHistory;
 
 public class AccountTest {
 	
-	private static Account test;
-	private final PrintStream originalOut = System.out;
-	public TransactionHistory transactionHistory = mock(TransactionHistory.class);
+	private Account test;
+	private ArrayList<HashMap<String, String>> mockedLog;
+//	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+//	private final PrintStream originalOut = System.out;
+	
+	@Mock
+	private TransactionHistory mockedTransactionHistory = mock(TransactionHistory.class);
 	
 	@Before
 	public void setUpStreams() {
-		System.setOut(new PrintStream(outContent));
-		test = new Account(transactionHistory);
-	}
+		test = new Account(mockedTransactionHistory);
+		// Add one deposit transaction to the mockedLog
+		mockedLog = new ArrayList<HashMap<String, String>>();
+		HashMap<String, String> mockedTransaction = new HashMap<String,String>(){{
+			put("Action", "Deposit");
+			put("Date", "01/01/2020");
+			put("Amount", "1000");
+		}};
+		mockedLog.add(mockedTransaction);
+	};
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void account_deposit_oneDeposit() {
+		when(mockedTransactionHistory.getLog()).thenReturn(mockedLog);
+		assertEquals(1, mockedLog.size());
+		assertEquals(mockedLog, test.deposit(1000, "01/01/2020"));
+	};
 
 }
